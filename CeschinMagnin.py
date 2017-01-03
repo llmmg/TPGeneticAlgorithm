848a479bfb7bc0feffdd6d9e06640d3cea7da2db
lancelot <lancelotlancelot94@gmail.com>
2017-01-03 10:23:42 +0100

    usage of objects Problem and Solutions. Comparison between objects

--- CeschinMagnin.py
+++ CeschinMagnin.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import math
 from random import randint
 
 
@@ -12,7 +13,7 @@ class City:
         self._point = point
 
     def __str__(self):
-        return self._name + " at position " + self._point
+        return self._name + " at position " + str(self._point)
 
     def point(self):
         return self._point
@@ -33,7 +34,7 @@ class Point:
         return str(self._x) + " ; " + str(self._y)
 
     def calculate_distance(self, point):
-        return pygame.math.sqrt((point.x() - self.x()) ** 2 + (point.y() - self.y()) ** 2)
+        return math.sqrt((point.x() - self.x()) ** 2 + (point.y() - self.y()) ** 2)
 
     def x(self):
         return self._x
@@ -43,6 +44,7 @@ class Point:
 
     def coords(self):
         return self._x, self._y
+
 
 # ----------------------------
 # Population
@@ -84,6 +86,9 @@ class Problem:
     def __init__(self, cities):
         self._cities = cities
 
+    def __eq__(self, prob):
+        return self._cities == prob.get_cities()
+
     def add_city(self, city):
         self._cities.append(city)
 
@@ -100,6 +105,9 @@ class Problem:
 class Solution:
     def __init__(self, problem):
         self._problem = problem
+
+    def __eq__(self, sol2):
+        return self._problem.get_cities() == sol2.problem().get_cities()
 
     def calculate_distance(self):
         return 0
@@ -208,6 +216,7 @@ def do(cities):
     # Generate random lists (rnd orders)
     # solution list
     solList = []
+    solList2 = []
     nbSolutions = 100
 
     # mix iterations
@@ -215,21 +224,27 @@ def do(cities):
 
     # generate 100 cities
     for i in range(0, nbSolutions):
-        newSol=generateRnd(cities, nbPass)
-        if newSol not in solList:
-            solList.append(newSol)
+        prob = Problem(generateRnd(cities, nbPass))
+        newSolution = Solution(prob)
+        if newSolution not in solList:
+            solList.append(newSolution)
+            print("appened")
 
     # Display generated solutions
     i = 0
     for sol in solList:
         i += 1
         print("solution ", i)
-        for cit in sol:
+        for cit in sol.problem().get_cities():
             print(cit)
+
+    # test
+    # print("distance from", cities[0], " to ", cities[1])
+    # print("dist=", cities[0].point().calculate_distance(cities[1].point()))
 
     # Natural selection => keep only x best solutions
     # keep 3/4 best
-    
+
 
 
     # Cross => childs
@@ -261,6 +276,11 @@ def do(cities):
 
 # return a list of rndm cities (= one solution)
 def generateRnd(cities, nPass):
+    """
+    :param cities: list of whole cities
+    :param nPass: number of mix pass. more passes = more random solutions
+    :return: a solution (list of cities in random order)
+    """
     sol = cities[:]
 
     # todo: 9% of chance to add the closest city next
